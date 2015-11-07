ba63c86b1676d8b05da766976286afa85defde9b
Carlos Saraiva <carlos.saraiva@msn.com>
2015-11-07 02:52:24 -0200

    Controllers improved

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var ghNotifier = angular.module('ghNotifier', ['ngRoute', ]);
+var ghNotifier = angular.module('ghNotifier', ['ngRoute', 'ghNotifier.websocket', 'ghNotifier.database']);
 
 ghNotifier.config(function ($routeProvider) {
     $routeProvider
@@ -16,73 +16,36 @@ ghNotifier.config(function ($routeProvider) {
         });
 });
 
-ghNotifier.service('db', ['$http', '$rootScope', '$q',
-
-    function ($http, $q) {
-        "use strict";
-
-        this.getData = function (action) {
-            return $http.get('https://ghnotifier.herokuapp.com/action/' + action)
-                .then(function (result) {
-                    return result;
-                }, function (error) {
-                    console.log(error);
-                    return $q.reject('Não foi possivel acessar o servidor.');
-                });
-        };
-
-    }]);
-
-ghNotifier.factory('IO', ['db', 'rootScope',
-    function (db, $rootScope) {
-        "use strict";
-        var socket = io("https://ghnotifier.herokuapp.com/");
-
-        socket.on('newuser', function (response) {
-            console.log(response.message + '\n' +
-                'id: ' + response.id + '\n' +
-                'ip:' + response.ip + ':' + response.port + '\n' +
-                'time: ' + response.timestamp);
-        });
-
-        socket.on('ondbgroupby', function () {
-            console.log('io - Event \'onnewrequest\' triggered.');
-            $rootScope.broadcast('oncounterupdated');
-            socket.emit('message ', {
-                message: 'onnewrequest received.'
-            });
-        });
-
-        socket.on('onnewrequest', function () {
-            console.log('io - Event \'ondbgroupby\' triggered.');
-            $rootScope.broadcast('onnewarrivedaction');
-            socket.emit('message ', {
-                message: 'ondbgroupby received.'
-            });
-
-        });
-
-        return socket;
-    }]);
-
-ghNotifier.controller('MainController', function ($scope, db, $rootScope) {
+ghNotifier.run(function (IO) {
     "use strict";
-
-    $rootScope.emit(db.getData('counter').then(function (result) {
-        $scope.counter = JSON.parse(result.data);
-    }, function (error) {
-        $scope.counter = error;
-    });
-
+    console.log('ghNotifier initialized!');
 });
 
-ghNotifier.controller('IssuesController', function ($scope, db) {
+
+ghNotifier.controller('MainController', function (db, $rootScope, $scope) {
+    "use strict";
+    var updateCounter = (function () {
+        db.getData('counter').then(function (result) {
+            $scope.counter = JSON.parse(result.data);
+        }, function (error) {
+            $scope.counter = error;
+        });
+    }());
+
+    $rootScope.$on('onnewrequest', updateCounter);
+});
+
+ghNotifier.controller('IssuesController', function ($scope, $rootScope, db) {
     "use strict";
 
-    db.getData('issues').then(function (result) {
-        $scope.counter = JSON.parse(result.data);
-    }, function (error) {
-        $scope.counter = error;
-    });
+    var getIssues = (function () {
+        db.getData('issues').then(function (result) {
+            $scope.actions = result.data;
+        }, function (error) {
+            $scope.issues = error;
+        });
+    }());
+
+    $rootScope.$on('onnewrequest', getIssues);
 
 });
